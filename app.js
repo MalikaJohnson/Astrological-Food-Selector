9f17543d248ff3f9d30b3b3f5db1d2ac7717774a
Malika Johnson <malikajohnson1995@MacBook-Air.local>
2021-04-20 13:57:41 -0400

    close... but no cigar

--- app.js
+++ app.js
@@ -4,21 +4,7 @@
 //  Button that removes astrological/food  data
 //  Append astrological blurb to the DOM
 //  Connect Astrological data to API recipes/food 
-const foodIds = {
-  "aries": 642372,
-  "taurus": 631750,
-  "gemini": 649030,
-  "cancer": 640279,
-  "leo": 659677,
-  "virgo": 644965,
-  "libra": 663357,
-  "scorpio": 631756,
-  "sag": 663144,
-  "cap": 636360,
-  "aquarius": 716381,
- "pisces": 1095994,
 
-}
 
 const DOMAIN = 'https://spoonacular.com/recipes/';
 const API_KEY = "1b80ef314c9144b099e149272c37d2a1"
@@ -30,11 +16,12 @@ searchBtn.addEventListener("click", getAll)
 
 
 async function getAll() {
-  const url = `https://api.spoonacular.com/recipes/${foodIds.aries}/information?includeNutrition=false&apiKey=${API_KEY}`
+  const foodBtn = document.querySelector('.foodBtn').value
+  const url = `https://api.spoonacular.com/recipes/${foodBtn}/information?includeNutrition=false&apiKey=${API_KEY}`
     try {
       const response = await axios.get(url)
       console.log(response.data)
-      addFood(response.data.search)
+      addFood(response.data)
       return response
 
   }
@@ -43,22 +30,23 @@ async function getAll() {
   }
 
 }
-getAll()
+// getAll()
 
-const addFood = (data) => {
-  data.forEach((food) => {
+const addFood = (food) => {
+  // data.forEach((food) => {
     const foodTitle = document.createElement("h3")
-    foodTitle.innerText = food.Title
+    foodTitle.innerText = food.title
+    console.log(food.title)
     document.querySelector(".foodData").append(foodTitle)
 
     const foodImage = document.createElement("img")
-    foodImage.src = food.Image
+    foodImage.src = food.image
     document.querySelector(".foodData").append(foodImage)
 
     const foodSourceUrl = document.createElement("h4")
-    foodSourceUrl.innerText = food.SourceUrl
+    foodSourceUrl.innerText = food.sourceUrl
     document.querySelector(".foodData").append(foodSourceUrl)
-  })
+  // })
 
 }
 
