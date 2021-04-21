bce3c6bea08de287370fdedd3ef71cdf78d2dc00
Malika Johnson <malikajohnson1995@MacBook-Air.local>
2021-04-21 15:53:30 -0400

    updated

--- app.js
+++ app.js
@@ -22,10 +22,19 @@ const foodExplain = [
     id: "649030", description:"So you're a Gemini... Playful and intellectually curious, Gemini is constantly juggling a variety of passions, hobbies, careers, and friend groups. They are the social butterflies of the zodiac and these quick-witted twins can talk to anyone about anything. A Korean beef bowl made up of different, and refreshing flavors and textures, is a perfect choice for this sign."
   },
   {
-    id: "640279", description:"So you're a Cancer"
+    id: "640279", description:"So you're a Cancer... Cancers tend to be domestically oriented. They love to create cozy, safe spaces that serve as their personal sanctuaries, then spend lots of time in them. They usually make excellent hosts and enjoy entertaining with comfort food and free-flowing libations. The delicate yet filling crab cake seems like a no brainer for this sign. "
   },
   {
-    id: "659677", description: "this is leo"
+    id: "659677", description:"So you're a Leo... These spirited fire signs are the kings and queens of the celestial jungle. Leos are renowned for their stability, loyalty, and consistency, and they enjoy cultivating friendships and romances that are artistically and creatively inspired. These astrological divas never get tired of lavish dinners, exclusive parties, or decadent designer wear, so it only makes sense that they would enjoy a lavish and beautifully plated scallop meal."
+  },
+  {
+    id: "644965", description:"So you're a Virgo... Virgos are logical, practical, and systematic in their approach to life. This earth sign is a perfectionist at heart and isn’t afraid to improve skills through diligent and consistent practice. Virgo rules the digestive system, which makes them especially attuned to the ingredients that make up a whole — in food and in everything else. They're hyper-aware of every detail. A crostini with well thought out layers of flavor, is the best fit for your sign."
+  },
+  {
+    id: "631814", description:"So you're a Libra... Libra is obsessed with symmetry and strives to create equilibrium in all areas of life. Libras adore high art, intellectualism, and connoisseurship. Suave libras need to surround themselves with stunning objects and create environments that reflect their exquisite tastes. Thats why a high value burger so appropriately named the $50,000 burger seems like a well suited choice for this sign."
+  },
+  {
+    id: "632026", description:"So you're a Scorpio... Life is a game of chess for these calculating water signs, who are constantly plotting several steps ahead in order to orchestrate an eventual checkmate. Scorpion energy is ambitious, enticing, and their enigmatic nature is what makes them so seductive and beguiling. That is why the surprisingly rich flavor of Ahi tuna that you will not find in any other fish, is the perfect choice for this sign."
   },
 ]
 
